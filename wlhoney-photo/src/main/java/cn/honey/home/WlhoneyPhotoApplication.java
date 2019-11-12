78840448cf388428961b0e049f875b7c3f485a27
ken.wangtj <ken.wangtj@homecredit.cn>
2019-11-12 18:34:56 +0800

    initial pom

--- wlhoney-photo/src/main/java/cn/honey/home/WlhoneyPhotoApplication.java
+++ wlhoney-photo/src/main/java/cn/honey/home/WlhoneyPhotoApplication.java
@@ -2,10 +2,10 @@ package cn.honey.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
 public class WlhoneyPhotoApplication {
 
     public static void main(String[] args) {
