ac4ef2171a0dd2679671aeeb58ba4214e03e68a8
ken.wangtj <ken.wangtj@homecredit.cn>
2019-11-19 16:43:21 +0800

    initial eureka

--- wlhoney-album/src/main/java/cn/honey/home/WLHoneyAlbumApplication.java
+++ wlhoney-album/src/main/java/cn/honey/home/WLHoneyAlbumApplication.java
@@ -2,10 +2,11 @@ package cn.honey.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
-//@EnableEurekaServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
 public class WLHoneyAlbumApplication {
 
     public static void main(String[] args) {
