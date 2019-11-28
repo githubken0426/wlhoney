881004f1d7b8d422b7c6b6c1024c6629bf69fd22
ken.wangtj <ken.wangtj@homecredit.cn>
2019-11-28 13:52:35 +0800

    initial pom

--- wlhoney-server-second/src/main/java/cn/honey/home/config/WebSecurityConfig.java
+++ wlhoney-server-second/src/main/java/cn/honey/home/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package cn.honey.home.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +11,12 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable(); //关闭csrf
+        super.configure(http); //开启认证
+    }
 
     /**
      * username:eureka-server
